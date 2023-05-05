afba0760e99af941d4457334a9eb22b8fca2a79b
LucasA75 <lucasacevedocastro@gmail.com>
2023-05-05 15:42:17 -0400

    Parte publica del proyecto

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output:'export',
-  basePath:'/admin',
+/*   output:'export',
+  basePath:'/SoloLicores',
   trailingSlash:true,
   images:{
     unoptimized:true
-  }
+  } */
 }
 
 module.exports = nextConfig
